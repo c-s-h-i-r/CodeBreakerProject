a6eb6fa83359792bda1b7e73603c1ffc16ea11d1
chris <commits@outlook.com>
2017-08-18 21:16:04 -0400

    try again

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,13 +14,13 @@ function setHiddenFields(){
     // Set hidden fields required for gameplay, ie. the answer
     let randomNumber = Math.floor(Math.random()*10000) // Randomly generated whole number [0-9999]
     //Make sure answer is always 4 digits
-    answer = ("0000" + randomNumber.toString()).slice(-4);
+    answer.value = ("0000" + randomNumber.toString()).slice(-4);
 
     // Initialize the number of attempts
-    attempt = 0;
+    attempt.value = 0;
 }
 
 function setMessage(msg){
     document.getElementById('message').innerHTML = msg;
-    
+
 }
