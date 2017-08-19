14f123db59e5a116c8b6f3b4dbb44bce9244bd59
chris <commits@outlook.com>
2017-08-18 20:05:36 -0400

    Created new function to generate answer for gameplay
    Changed name of main page.

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,11 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+
 }
 
-//implement new functions here
+function setHiddenFields(){
+    // Set hidden fields required for gameplay, ie. the answer
+    let answer = Math.floor(Math.random()*10000) // Randomly generated whole number [0-9999]
+    
+}
